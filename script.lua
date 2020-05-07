d2f2d6a31ec2542977168edb866224c32b2bd0eb
BGERGO7 <38499123+BGERGO7@users.noreply.github.com>
2020-05-07 12:08:38 +0200

    Player spawn

--- script.lua
+++ script.lua
@@ -1,44 +1,7 @@
---[[
-addEventHandler("onClientKey", getRootElement(), 
-    function(key, state)
-        if state then
-            outputChatBox("Gomb lenyomva")
-        else 
-            outputChatBox("Gomb felengedve")
-        end 
+-- ha a player lespawnol
+    function onPlayerSpawnHandler ( )
+        local playerName = getPlayerName( source )
+        outputChatBox ( playerName .. " üdvözöllek a szerveren!" )
     end
-)
-
-
-for i = 0, 50 do
-    outputChatBox(i)
-end 
-
-local urestabla = {"Ez", "Egy", "Mondat"}
-outputChatBox(#urestabla) -- 3
-outputChatBox(urestabla[1]) -- "Ez"
-
-local urestabla2 = {
-    ["elso_elem"] = {"Ez ","egy", "mondat"}
-}
-
-outputChatBox(urestabla2["elso_elem"][1]) -- Ez
-
-local teszt_tabla = {"Ez", "egy","mondat"}
-
-for k, v in pairs(teszt_tabla) do
-    outputChatBox(k.. " " ..v) --pl: 1 Ez 2 egy 3 mondat
-end 
-
-local i = 0
-
-while i < 10 do
-    i = i + 1
-    outputChatBox(i) -- 1 - 10
-end 
-
-]]--
-
-addCommandHandler(Valami, valami)
-
-valami()
+    
+   addEventHandler("Player spawn", getRootElement() , onPlayerSpawnHandler)
